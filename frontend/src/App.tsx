084d4c6e555162b50b1d4e5c0b1e2504e4faeb98
Adam Gajzlerowicz <nelf86@gmail.com>
2020-01-26 17:55:40 +0100

    added empty frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+// import {Post, PostWhereInput} from '../../api/src/prisma';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const EXCHANGE_RATES = gql`
+  {
+    posts {
+      id
+      text
+    }
+  }
+`;
+
+export default function App() {
+  // const { loading, error, data } = useQuery<Post[], PostWhereInput>(EXCHANGE_RATES);
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  if (!data) {
+   return null
+  }
+
+
+  // @ts-ignore
+  return (data.posts || []).map((post: Post) => (
+    <div key={post.id}>
+      <p>
+        {post.id}: {post.text}
+      </p>
     </div>
-  );
+  ));
 }
 
-export default App;
